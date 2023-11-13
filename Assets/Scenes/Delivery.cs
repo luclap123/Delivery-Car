b5b49e9cc031cafd95487bdbc3562fbde09e3fa6
Lục Lập <boylap7@gmail.com>
2023-11-13 15:12:21 +0700

    change color
    làm cho xe đổi màu sắc khi đi tới điểm chứa package và địa điểm nhận package

--- Assets/Scenes/Delivery.cs
+++ Assets/Scenes/Delivery.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {   
     bool hasPackage;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 hasNoPackageColor = new Color32(0, 0, 0, 1);
     [SerializeField] float timeDestroy;
+    SpriteRenderer spriteRenderer;
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
         // Debug.Log("You touch a Circle");
@@ -15,6 +21,7 @@ public class Delivery : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Package" && !hasPackage)
         {
+            spriteRenderer.color = hasPackageColor;
             Debug.Log("You picked a package");
             hasPackage=true;
             Destroy(other.gameObject, timeDestroy);
@@ -24,6 +31,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("successfully picked a location");
             hasPackage=false;
+            spriteRenderer.color = hasNoPackageColor;
         }
     }
 }
